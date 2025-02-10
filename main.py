a0bc287b86524743138bbf45bf297c020cd36ad6
elinefarias <eline.farias33@gmail.com>
2025-02-10 17:49:15 -0300

    version-final-01 completa

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request, HTTPException
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from pydantic import BaseModel
@@ -9,10 +9,19 @@ from server.gunicorn_server import GunicornServer
 from fastapi import File, UploadFile
 from datetime import datetime
 import numpy as np
+import pandas as pd
 import openai
 import os
 import json
 import PyPDF2
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.preprocessing import normalize
+import pdfkit
+from fastapi.encoders import jsonable_encoder
+from weasyprint import HTML
+
+
 
 app = FastAPI(
     title="Provoc",
@@ -37,8 +46,15 @@ def extract_text_from_pdf(pdf_path):
     return text
 
 
-def calcular_perfil_dominante(dados):
+def get_embeddings(texts):
+    response = openai.Embedding.create(
+        model="text-embedding-ada-002",
+        input=texts
+    )
+    embeddings = [embedding['embedding'] for embedding in response['data']]
+    return np.array(embeddings)
 
+def calcular_perfil_dominante(dados):
     mapeamento_perguntas = {
         1: "R", 2: "R",
         3: "I", 4: "I",
@@ -49,12 +65,20 @@ def calcular_perfil_dominante(dados):
     }
 
     pontuacoes = {"R": 0, "I": 0, "A": 0, "S": 0, "E": 0, "C": 0}
+    
+    mapeamento_respostas = {
+        "Concordo totalmente": 5,
+        "Concordo parcialmente": 4,
+        "Neutro": 3,
+        "Discordo parcialmente": 2,
+        "Discordo totalmente": 1
+    }
 
     # Processa as respostas para calcular a pontuação por categoria
     for resposta in dados["respostas"]:
         pergunta = resposta["pergunta"]
         categoria = mapeamento_perguntas[pergunta]
-        pontuacoes[categoria] += int(resposta["resposta"])
+        pontuacoes[categoria] += mapeamento_respostas[resposta["resposta"]]
 
     # Identifica a categoria com a maior pontuação (perfil dominante)
     perfil_dominante_letra = max(pontuacoes, key=pontuacoes.get)
@@ -69,7 +93,6 @@ def calcular_perfil_dominante(dados):
         "C": "Convencional"
     }
 
-    # Descrição dos perfis
     descricao_perfis = {
         "R": "Preferência por trabalhos práticos, manuais e técnicos.",
         "I": "Interesse por pesquisa, análise e ciência.",
@@ -91,9 +114,9 @@ def calcular_perfil_dominante(dados):
         "perfil_dominante": perfil_dominante,
         "pontuacao": pontuacoes[perfil_dominante_letra]
     }
+    
     print("Perfil dominante calculado:", resultado)
     return resultado
-
 
 
 
@@ -103,7 +126,8 @@ def agente_provoc(prompt, nome, perfil_dominante):
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": "Você é um especialista em orientação vocacional."},
-            {"role": "user", "content": f"O perfil dominante é {perfil_dominante['nome']}"},
+            {"role": "user", "content": f"O perfil Primário é {perfil_dominante['nome']}"},
+            {"role": "user", "content": "Por favor, forneça informações detalhadas sobre carreiras adequadas para este perfil, incluindo possíveis áreas de interesse, habilidades necessárias e dicas de desenvolvimento pessoal."},
             {"role": "user", "content": prompt}
         ]
     )
@@ -121,7 +145,6 @@ def agente_provoc(prompt, nome, perfil_dominante):
         json.dump({"report_text": report_text}, f)
     
     return {"message": "Relatório gerado com sucesso"}
-
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -149,7 +172,7 @@ async def calcular_perfil(response: QuestionnaireResponse):
 
 
 @app.get("/relatorio.html", response_class=HTMLResponse)
-async def get_report_page(request: Request, nome: str):
+async def get_report_page(request: Request, nome: str, perfil_primario: str, descricao_primaria: str,  cursos_recomendados: str = ""):
     template = env.get_template('relatorio.html')
     report_json_path = f"database/{nome}_relatorio.json"
     
@@ -161,10 +184,23 @@ async def get_report_page(request: Request, nome: str):
         report_text = "Relatório não encontrado."
     
     data_atual = datetime.now().strftime("%d/%m/%Y")
+    cursos_recomendados_list = cursos_recomendados.split(",") if cursos_recomendados else []
     
-    return template.render(request=request, nome=nome, report_text=report_text, data=data_atual)
-
-
+    html_content = template.render(
+        request=request, 
+        nome=nome, 
+        report_text=report_text, 
+        data=data_atual, 
+        perfil_primario=perfil_primario, 
+        descricao_primaria=descricao_primaria,
+        cursos_recomendados=cursos_recomendados_list
+    )
+    pdf_path = f"database/{nome}_relatorio.pdf"
+    HTML(string=html_content).write_pdf(pdf_path)
+    
+    return html_content
+    
+    
 @app.post("/agente_provoc")
 async def agente_provoc_endpoint(request: Request):
     data = await request.json()
@@ -174,6 +210,45 @@ async def agente_provoc_endpoint(request: Request):
     resultado = agente_provoc(prompt, nome, perfil_dominante)
     return resultado
 
+
+@app.post("/recomendar_cursos")
+async def recomendar_cursos(response: QuestionnaireResponse):
+    data = response.model_dump()  
+    respostas_usuario = " ".join([str(resposta["resposta"]).lower() for resposta in data.get("respostas", [])])
+
+    df = pd.read_csv('database/cursos_superiores.csv', delimiter=';', encoding='utf-8')
+    detalhes_cursos = df['Detalhes'].apply(lambda x: str(x).lower()).tolist()
+
+    # Geração de embeddings
+    documentos = detalhes_cursos + [respostas_usuario]  # Último documento são as respostas do usuário
+    embeddings = get_embeddings(documentos)
+
+    # Busca de similaridade
+    query_vector = embeddings[-1].reshape(1, -1)  # Vetor de consulta (respostas do usuário)
+    embeddings = embeddings[:-1]  # Embeddings dos cursos
+
+    # Cálculo da similaridade cosseno
+    similarities = cosine_similarity(query_vector, embeddings)[0]
+
+    # Gerar a lista de cursos recomendados com base na similaridade
+    cursos_recomendados = [
+        {"curso": df.iloc[i]['Nome do curso'], "similaridade": f"{similaridade:.2f}"}
+        for i, similaridade in enumerate(similarities)
+    ]
+
+    # Ordenar os cursos recomendados pela similaridade (maior para menor)
+    cursos_recomendados.sort(key=lambda x: float(x["similaridade"]), reverse=True)
+    
+    # Limitar a lista a três cursos
+    cursos_recomendados = cursos_recomendados[:3]
+    
+    # Retornar a resposta com os cursos recomendados
+    json_data = jsonable_encoder({
+        "message": "Recomendação gerada com sucesso!",
+        "cursos_recomendados": cursos_recomendados,
+        "detalhes": "Os cursos estão listados em ordem de compatibilidade com suas respostas."
+    })
+    return JSONResponse(content=json_data)
 
 
 if __name__ == "__main__":
